2932d71c76435b8c38e722adafdd568a1465b443
alokshandilya <alok.shandilya10@gmail.com>
2024-10-13 08:07:11 +0530

    chore: add links app, templates, and views
    create links app with templates and views, use template inheritance to
    create a base template and index template

--- links/views.py
+++ links/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Link
+
 
 # Create your views here.
+def index(request):
+    links = Link.objects.all()
+    context = {
+        "links": links,
+    }
+    return render(request, "links/index.html", context)
